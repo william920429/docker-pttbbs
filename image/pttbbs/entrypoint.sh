3df3fd2d16e5c20a49435af1911423202bea4fb5
william920429 <william920429@gmail.com>
2026-02-10 00:44:48 +0800

    fix cron and use "no new privileges"

--- image/pttbbs/entrypoint.sh
+++ image/pttbbs/entrypoint.sh
@@ -4,13 +4,14 @@ set -eu
 exec_as(){
     exec \
         env HOME=/home/bbs SHELL=/usr/sbin/nologin USER=bbs LOGNAME=bbs \
-        setpriv --reuid bbs --regid bbs --clear-groups -- \
+        setpriv --reuid bbs --regid bbs --clear-groups --no-new-privs -- \
         "$@"
 }
 
 setup_bbs_home(){
     if [ -z "$(ls -A "/home/bbs")" ]; then
         cp -a /usr/local/pttbbs/etc /home/bbs/etc
+        mkdir /home/bbs/backup
         ln -srnf /usr/local/pttbbs/bin /home/bbs/bin
         ln -srnf /dev/shm /home/bbs/run
         chown --no-dereference -R bbs:bbs /home/bbs/
